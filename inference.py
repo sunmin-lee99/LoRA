df36696bdcc9b07551a518663aec0921585d37e5
Sunmin Lee <79503414+sunmin-lee99@users.noreply.github.com>
2024-01-16 21:08:45 +0900

    Add files via upload

---
+++ inference.py
@@ -0,0 +1,25 @@
+from diffusers import StableDiffusionPipeline
+import torch
+
+device = "cuda"
+
+# load model
+model_path = "./output/pytorch_lora_weights.bin"
+pipe = StableDiffusionPipeline.from_pretrained(
+    "runwayml/stable-diffusion-v1-5",
+    torch_dtype=torch.float16,
+    safety_checker=None,
+    feature_extractor=None,
+    requires_safety_checker=False
+)
+
+# load lora weights
+pipe.unet.load_attn_procs(model_path)
+# set to use GPU for inference
+pipe.to(device)
+
+# generate image
+prompt = "a drawing of a white rabbit"
+image = pipeline(prompt, num_inference_steps=30).images[0]
+# save image
+image.save("image.png")
